eaaead8cf74c1530d99c898f054f12aa38384773
Jesus Bermudez <jesmanuelber@gmail.com>
2024-02-07 14:29:21 -0500

    MOD: Modifying translate command

--- src/commands/translate.js
+++ src/commands/translate.js
@@ -1,18 +1,27 @@
 import { translate } from "@vitalets/google-translate-api";
 
 export default async function translator(ctx) {
-  // text after the command
-  const payload = ctx.payload.trim();
-
-  // if is empty
-  if (payload.length < 1) return;
+  const message = ctx.update.message; // message object
+  const payload = ctx.payload.trim(); // text after the command
+  let text = ""; // traduced text
+  let replyId = message.message_id;
 
   // translate
   try {
-    const { text } = await translate(payload, { to: "es" });
+    // if is reply to another message
+    if (message.reply_to_message && message.reply_to_message.text) {
+      text = (await translate(message.reply_to_message.text, { to: "es" }))
+        .text;
+      replyId = message.reply_to_message.message_id;
+    } else if (payload.length > 0) {
+      // if isn't empty
+      text = (await translate(payload, { to: "es" })).text;
+    } else {
+      return;
+    }
 
     ctx.reply(`🌐: ${text}`, {
-      reply_to_message_id: ctx.update.message.message_id,
+      reply_to_message_id: replyId,
     });
   } catch (error) {
     console.log(error);
