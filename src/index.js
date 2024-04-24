ca2388709ad92de75a65728fbdcc7a697718259c
Jesus Bermudez <jesmanuelber@gmail.com>
2024-04-24 14:59:49 -0500

    .

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import laughter from "./commands/laughter.js";
 import createdCommands from "./commands/createdCommands.js";
 import translator from "./commands/translate.js";
 import weather from "./commands/weather.js";
-import { currentValue, foreignExchange } from "./commands/exchangeRate.js";
+import { foreignExchange } from "./commands/exchangeRate.js";
 
 const bot = new Telegraf(process.env.token);
 
