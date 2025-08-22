afc87abd8841c259313b5a972b4e5592647a31ca
Jesus Bermudez <jesmanuelber@gmail.com>
2025-08-22 06:35:40 -0500

    handle message text, cmd condition fixed

--- src/utils/handleMessageText.js
+++ src/utils/handleMessageText.js
@@ -3,11 +3,11 @@ export default function handleMessageText(message) {
   const textArray = text.trim().split(/\s+/);
   let cmd = textArray[0];
 
-  if (cmd.startsWith("/") && textArray.length > 1) {
+  if (cmd.startsWith("/") && cmd.length > 1) {
     text = textArray.slice(1).join(" ");
   }
 
-  if (!cmd.startsWith("/")) cmd = "";
+  if (!cmd.startsWith("/") || cmd.length == 1) cmd = "";
 
   return {
     cmd,
